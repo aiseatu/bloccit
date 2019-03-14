7f3291a49dc6dfc67d49ec761b9fb85979784627
Xiaokang Tang <xiaokangt@hotmail.com>
2019-03-14 18:17:47 -0400

    checkpoint authentication complete

--- spec/integration/users_spec.js
+++ spec/integration/users_spec.js
@@ -57,7 +57,7 @@ describe("routes: users", () => {
     it("should not create a new user with invalid attributes and redirect", (done) => {
       request.post(
         {
-          url: case,
+          url: base,
           form: {
             email: "no",
             password: "123456789"
@@ -75,6 +75,18 @@ describe("routes: users", () => {
           });
         }
       );
+    });
+
+  });
+
+  describe("GET /users/sign_in", () => {
+
+    it("should render a view with a sign in form", (done) => {
+      request.get(`${base}sign_in`, (err, res, body) => {
+        expect(err).toBeNull();
+        expect(body).toContain("Sign in");
+        done();
+      });
     });
 
   });
